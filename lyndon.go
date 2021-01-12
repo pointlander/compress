f1390ed9e1af61078b0c1820f15a66b8b6c796fb
Andrew Snodgrass <git@pointlander.info>
2021-01-12 10:15:36 -0700

    Fixed lyndon bug

--- lyndon.go
+++ lyndon.go
@@ -5,27 +5,32 @@
 package compress
 
 type Lyndon struct {
-        Words [][]uint8
+	Words [][]uint8
 }
 
 func (l *Lyndon) Factor(s []uint8) {
-        k, m, n, words, max := 0, 1, len(s), l.Words[:0], len(s)
-	if max > 256 {
-		max = 256 + (max - 256) / 2
+	k, m, n, words, max := 0, 1, len(s), l.Words[:0], len(s)
+	if max == 0 {
+		panic("len(s) should be > 0")
+	} else if max == 1 {
+		l.Words = append(words, s)
+		return
+	} else if max > 256 {
+		max = 256 + (max-256)/2
 	}
 	if cap(words) < max {
 		words = make([][]uint8, 0, max)
 	}
 
-        for {
+	for {
 		switch sk, sm := s[k], s[m]; true {
 		case sk < sm:
-			k, m = 0, m + 1
+			k, m = 0, m+1
 			if m < n {
 				continue
 			}
 		case sk == sm:
-			k, m = k + 1, m + 1
+			k, m = k+1, m+1
 			if m < n {
 				continue
 			}
@@ -37,8 +42,8 @@ func (l *Lyndon) Factor(s []uint8) {
 			if n > 1 {
 				continue
 			}
-                }
+		}
 		break
-        }
+	}
 	l.Words = append(words, s)
 }
